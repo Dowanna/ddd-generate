12b472a84d1c1adac1b4f4009d8e1f95ba6e06ca
shunya matsubara <shun.fzs1000@gmail.com>
2020-08-16 10:31:43 +0900

    :+1: kebab case

--- src/util/string.ts
+++ src/util/string.ts
@@ -6,6 +6,9 @@ export const lowerCaseFirstChar = (text: string) => {
 }
 
 export const kebabCase = (text: string) => {
-  // fixme: ちゃんとケバブケースに変換する
-  return text.toLowerCase()
+  const _text = text
+  return _text
+  .replace(/([a-z])([A-Z])/g, '$1-$2')    // get all lowercase letters that are near to uppercase ones
+  .replace(/[\s_]+/g, '-')                // replace all spaces and low dash
+  .toLowerCase()                          // convert to lower case
 }
